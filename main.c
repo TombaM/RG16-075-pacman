7c962a3f994991bb464572032f800750961c1450
Tomislav <tomicass3@gmail.com>
2017-08-22 21:36:33 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
-// #include "arena.h"
 #include "draw.h"
 
 #define WINDOW_HEIGHT 700
@@ -23,6 +22,9 @@ static void on_reshape(int width, int height);
 
 /* Deklaracija pomocnih funkcija */
 void get_map_pattern();
+
+int x_cord = 1;
+int y_cord = 1;
 
 int main(int argc, char** argv) {
   /* Ucitavanje map patterna */
@@ -60,16 +62,43 @@ static void on_display(void) {
   /* Podesavanje vidne tacke */
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  gluLookAt(4, 7, -2, 4, 2, -4, 0, 1, 0);
+
+			gluLookAt(0, 30, 0, 0, 0, -15, 0, 1, 0);
+
 
   int i, j;
+  int pacman_drawn = 0;
 
-  for(i = 0; i < MAP_HEIGHT; i++) {
-    for(j = 0; j < MAP_WIDTH; j++) {
-      if(map[i][j] == 'o') {
-        draw_blue_cube(0.4*i, 0.4*(i+1), -0.2, 0.2, -0.4*j, -0.4*(j+1));
+  for(i = 1; i <= MAP_HEIGHT; i++) {
+    for(j = 1; j <= MAP_WIDTH; j++) {
+      if(map[i-1][j-1] == 'o') {
+        //draw_blue_cube(0.4*i, 0.4*(i+1), -0.2, 0.2, -0.4*j, -0.4*(j+1));
         // draw_blue_cube(0.2*i, 0.2*(i+1), -0.1, 0.1, -0.2*j, -0.2*(j+1));
+      	glPushMatrix();
+                glTranslatef(i, 0, -j);
+                glColor3f(0, 0, 1);
+		glutSolidCube(1);
+	glPopMatrix();
+      }
+      else if(map[i-1][j-1] == 'x') {
+        //draw_floor(0.4*i, 0.4*(i+1), -0.2, -0.4*j, -0.4*(j+1));
+        glPushMatrix();
+        glTranslatef(i, 0, -j);
+        glColor3f(256/226, 256/219, 256/172);
+		glScalef(1, 0.001, 1);
+		glutSolidCube(1);
+	glPopMatrix();
 
+    if(pacman_drawn == 0)
+    {
+      pacman_drawn = 1;
+      glPushMatrix();
+      glColor3f(256/196, 256/173, 0);
+        glTranslatef(x_cord*i, 0, -j*y_cord);
+        glScalef(x_cord, 1, y_cord);
+        glutSolidSphere(.5, 100, 100);
+      glPopMatrix();
+    }
       }
     }
   }
@@ -83,6 +112,15 @@ static void on_keyboard(unsigned char key, int x, int y) {
     case 27:
       exit(0);
       break;
+
+      case 'w':
+      case 'W':
+        y_cord += 1;
+        break;
+
+        case 'd':
+        x_cord += 1;
+        break;
   }
 }
 
@@ -92,11 +130,15 @@ static void on_reshape(int width, int height) {
 
   /* Podesava se viewport. */
   glViewport(0, 0, window_width, window_height);
+//   /* Podesavanje projekcije */
+//   glMatrixMode(GL_PROJECTION);
+//   glLoadIdentity();
+//   gluPerspective(80, window_width/(float)window_height, 1, 100);
 
-  /* Podesavanje projekcije */
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(80, window_width/(float)window_height, 1, 100);
+  	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60, width/(double)height, 1, 100);
 }
 void get_map_pattern() {
   FILE* file;
